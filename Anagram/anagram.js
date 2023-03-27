45f22c78d8272f10223612df95a78764a0a93af9
Shawna <skjacques+school@gmail.com>
2023-03-27 12:02:31 -0700

    Anagram.

---
+++ Anagram/anagram.js
@@ -0,0 +1,15 @@
+// Check whether two Strings are anagram of each other
+// Input: str1 = “listen”  str2 = “silent”
+// Output: “Anagram”
+
+// elbow below
+
+// Input: str1 = “gram”  str2 = “arm”
+// Output: “Not Anagram”
+
+
+// Situation: Set the scene and give the necessary details of your example.
+// Task: Describe what your responsibility was in that situation.
+// Action: Explain exactly what steps you took to address it.
+// Result: Share what outcomes your actions achieved.
+
